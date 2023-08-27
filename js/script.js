dc7e73aa17e4d71269ae9cb4269a6331add723ca
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-27 20:04:26 +0500

    debug

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 console.log("JS script integrated");
 
 function openpage() {
+    console.log("window.location ====", window.location);
     setTimeout(() => {
         window.location.href = '/pages/auth/signup.html';
 
