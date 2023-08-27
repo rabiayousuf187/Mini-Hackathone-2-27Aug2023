b0f3135d9b9cf9d929ea52019cd0cb2d033842a1
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-27 13:15:09 +0500

    Blog successfully save to firebase

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -4,10 +4,13 @@ let userAcc = isAuth();
 console.log("userAcc get via is Auth()", userAcc);
 
 if (userAcc && userAcc.acc_type === "blogger") {
-
+  
   console.log("Dashboard Page")
   document.getElementById("Top").style.display = "block";
-    document.getElementById("adminname").innerText = userAcc.firstname + lastname;
+  document.getElementById("adminname").innerText = userAcc.firstname + " "+ userAcc.lastname;
+  
+  let userId = userAcc.id;
+  console.log("userAcc Id", userId);
   // Use the Firebase Configuration functions
   const {
     database,
@@ -27,111 +30,111 @@ if (userAcc && userAcc.acc_type === "blogger") {
   const isFirstLoad = JSON.parse(localStorage.getItem("isUserFirstLoad"));
   //
   window.addEventListener("load", () => {
-    // let getCartData = getAllItemData(`cart/${userAcc.id}/`);
+    let getCartData = getAllItemData(`cart/${userAcc.id}/`);
 
-    // getAllItemData(`items/fruit/`)
-    //   .then((itemsData) => {
-    //     if (!itemsData) {
-    //       console.log("Data is null");
-    //     } else {
-    //       // Here you can continue with rendering your data or performing other tasks
-    //       console.log("updated into Array ====:", itemsData);
-    //       selectedCategory = "fruit";
-    //       const container = document.getElementById("show-item-inner");
-    //       getSelectedItemData(`cart/${userAcc.id}/`)
-    //         .then((cartData) => {
-    //           console.log(`GET CART DATA ==`, cartData);
-    //           disableItem = Object.keys(cartData);
+    getAllItemData(`items/fruit/`)
+      .then((itemsData) => {
+        if (!itemsData) {
+          console.log("Data is null");
+        } else {
+          // Here you can continue with rendering your data or performing other tasks
+          console.log("updated into Array ====:", itemsData);
+          selectedCategory = "fruit";
+          const container = document.getElementById("show-item-inner");
+          getSelectedItemData(`cart/${userAcc.id}/`)
+            .then((cartData) => {
+              console.log(`GET CART DATA ==`, cartData);
+              disableItem = Object.keys(cartData);
 
-    //           console.log("CART KEY ==== ", disableItem);
+              console.log("CART KEY ==== ", disableItem);
 
-    //           console.log("disableItem ==== ", disableItem);
-    //           itemsData.forEach((ele, ind) => {
-    //             const matchExists = disableItem.some(
-    //               (item) => item.toLowerCase() === ele.itemName.toLowerCase()
-    //             );
+              console.log("disableItem ==== ", disableItem);
+              itemsData.forEach((ele, ind) => {
+                const matchExists = disableItem.some(
+                  (item) => item.toLowerCase() === ele.itemName.toLowerCase()
+                );
 
-    //             if (matchExists) {
-    //               console.log(`${ele.itemName} found in the itemData.`);
-    //               // Call the function to add a fruit item
-    //               // name, weight, price, imageURL
-    //               showItem(
-    //                 container,
-    //                 ind,
-    //                 ele.itemCategory,
-    //                 ele.itemName,
-    //                 ele.unitName,
-    //                 ele.unitPrice,
-    //                 ele.imageUrl,
-    //                 ele.itemContent,
-    //                 true
-    //               );
-    //             } else {
-    //               showItem(
-    //                 container,
-    //                 ind,
-    //                 ele.itemCategory,
-    //                 ele.itemName,
-    //                 ele.unitName,
-    //                 ele.unitPrice,
-    //                 ele.imageUrl,
-    //                 ele.itemContent,
-    //                 false
-    //               );
-    //               console.log(`${ele.itemName} not found in the array.`);
-    //             }
-    //             // console.log("Each Item ==== :", ele);
-    //             console.log(
-    //               "Each Item ==== :",
-    //               ele.itemCategory,
-    //               ele.itemName,
-    //               ele.unitName,
-    //               ele.unitPrice,
-    //               ele.imageUrl
-    //             );
+                if (matchExists) {
+                  console.log(`${ele.itemName} found in the itemData.`);
+                  // Call the function to add a fruit item
+                  // name, weight, price, imageURL
+                  showItem(
+                    container,
+                    ind,
+                    ele.itemCategory,
+                    ele.itemName,
+                    ele.unitName,
+                    ele.unitPrice,
+                    ele.imageUrl,
+                    ele.itemContent,
+                    true
+                  );
+                } else {
+                  showItem(
+                    container,
+                    ind,
+                    ele.itemCategory,
+                    ele.itemName,
+                    ele.unitName,
+                    ele.unitPrice,
+                    ele.imageUrl,
+                    ele.itemContent,
+                    false
+                  );
+                  console.log(`${ele.itemName} not found in the array.`);
+                }
+                // console.log("Each Item ==== :", ele);
+                console.log(
+                  "Each Item ==== :",
+                  ele.itemCategory,
+                  ele.itemName,
+                  ele.unitName,
+                  ele.unitPrice,
+                  ele.imageUrl
+                );
 
-    //             // const found = array.includes(searchString);
+                // const found = array.includes(searchString);
 
-    //             const lazyImages = document.querySelectorAll(".lazy-image");
-    //             const loadImagePromises = [];
-    //             lazyImages.forEach((img) => {
-    //               const promise = new Promise((resolve) => {
-    //                 img.addEventListener("load", () => {
-    //                   resolve();
-    //                 });
-    //                 img.src = img.getAttribute("data-src");
-    //               });
-    //               loadImagePromises.push(promise);
-    //             });
-    //             Promise.all(loadImagePromises)
-    //               .then(() => {
-    //                 console.log("All lazy-loaded images are loaded.");
-    //               })
-    //               .catch((error) => {
-    //                 console.error("An error occurred:", error);
-    //               });
-    //           });
+                const lazyImages = document.querySelectorAll(".lazy-image");
+                const loadImagePromises = [];
+                lazyImages.forEach((img) => {
+                  const promise = new Promise((resolve) => {
+                    img.addEventListener("load", () => {
+                      resolve();
+                    });
+                    img.src = img.getAttribute("data-src");
+                  });
+                  loadImagePromises.push(promise);
+                });
+                Promise.all(loadImagePromises)
+                  .then(() => {
+                    console.log("All lazy-loaded images are loaded.");
+                  })
+                  .catch((error) => {
+                    console.error("An error occurred:", error);
+                  });
+              });
 
-    //           const inactive = document.querySelectorAll(".cat-row");
+              const inactive = document.querySelectorAll(".cat-row");
 
-    //           inactive.forEach((div) => {
-    //             console.log("check DIv === ", div);
-    //             if (div.hasAttribute("disabled")) {
-    //               div.classList.add("disable");
-    //               console.log("disable DIv === ", div);
-    //             }
-    //           });
-    //         })
-    //         .catch((error) => {
-    //           console.error("Error Getting Cart Items Data:", error);
-    //         });
-    //     }
-    //     // Process the retrieved data
-    //   })
+              inactive.forEach((div) => {
+                console.log("check DIv === ", div);
+                if (div.hasAttribute("disabled")) {
+                  div.classList.add("disable");
+                  console.log("disable DIv === ", div);
+                }
+              });
+            })
+            .catch((error) => {
+              console.error("Error Getting Cart Items Data:", error);
+            });
+        }
+        // Process the retrieved data
+      })
 
-    //   .catch((error) => {
-    //     console.error("Error fetching Items Data:", error);
-    //   });
+      .catch((error) => {
+        console.error("Error fetching Items Data:", error);
+      });
   });
 
   let getSelectedItemData = async (url) => {
@@ -158,23 +161,146 @@ if (userAcc && userAcc.acc_type === "blogger") {
       logout();
     }, 1000);
   });
-  // let current_page = document.querySelector('#home');
-  // console.log("current_page color change", current_page);
-  // current_page.querySelector("img").style.filter =
-  //   "invert(62%) sepia(112%) saturate(349%) hue-rotate(61deg) brightness(56%) contrast(168%)";
-  // current_page.querySelector("p").style.color = "#61B846";
 
-  // let cart = document.getElementById("cart");
-  // cart.addEventListener("click", function (event) {
-  //   event.preventDefault(); // Prevent the default link behavior
-  //   window.location.href = "./shop-cart.html";
-  // });
+  const signupForm = document.getElementById("signup-form");
 
-  // let acc_setting = document.getElementById("acc-setting");
-  // acc_setting.addEventListener("click", function (event) {
-  //   event.preventDefault(); // Prevent the default link behavior
-  //   window.location.href = "./account-setting.html";
-  // });
+
+  // Function to display error message for an input field
+  function showError(inputElement, errorMessage) {
+    const errorElement = document.getElementById(inputElement.id + "Error");
+    errorElement.textContent = errorMessage;
+
+    // Add the .error class to the input element
+    errorElement.classList.add("error");
+    console.log(
+      " inputElement.classList.add('error') = ",
+      errorElement,
+      errorElement.classList.add("error")
+    );
+  }
+
+  // Function to clear error message for an input field
+  function clearError(inputElement) {
+    const errorElement = document.getElementById(inputElement.id + "Error");
+    errorElement.textContent = "";
+
+    // Remove the .error class from the input element
+    errorElement.classList.remove("error");
+    console.log(
+      "inputElement.classList.remove('error'); = ",
+      errorElement.classList.remove("error")
+    );
+  }
+
+
+    // Get Category Data for admin
+    let getAllItemData = async () => {
+      try {
+        const snapshot = await get(ref(database, `categories/`));
+        // Data snapshot contains the data at the specified location
+        let itemsData = snapshot.val();
+        console.log("Retrieved data:", itemsData);
+        itemsData = Object.values(itemsData);
+        return itemsData;
+      } catch (error) {
+        console.error("Error getting data:", error);
+        alert(error);
+        return false;
+      }
+    };
+
+  // Function to validate the form on submission
+  function validateForm(event) {
+    event.preventDefault();
+
+    const blogtitle = document.getElementById("blogtitle").value;
+    const blogcontent = document.getElementById("blogcontent").value;
+
+    let acc_type, userAcc;
+    // let acc_type = document.querySelector('input[name="acc_type"]:checked');
+
+    console.log("blogtitle = ", blogtitle);
+    console.log("blogcontent = ", blogcontent);
+
+    if (blogtitle.trim() === "") {
+      showError(
+        document.getElementById("blogtitle"),
+        "blogtitle is required."
+      );
+    } else {
+      clearError(document.getElementById("blogtitle"));
+    }  
+      if (blogcontent.trim() === "") {
+      showError(
+        document.getElementById("blogcontent"),
+        "blogcontent is required."
+      );
+    } else {
+      clearError(document.getElementById("blogcontent"));
+    }
+
+    console.log(
+      "!document.querySelector.error ==== ",
+      document.querySelector("#signup-form")
+    );
+    console.log(
+      "!document.querySelector.error ==== ",
+      !document.querySelector(".error")
+    );
+    if (!document.querySelector(".error")) {
+      if (
+        !blogtitle ||
+        !blogcontent
+      ) {
+        alert("Refill Form for all Feilds\nSome Feilds are undefined.");
+        console.log("Refill Form for all Feilds\nSome Feilds are undefined.");
+      } else {
+      // Submit the form or do any other required action here
+      console.log("Form submitted successfully!");
+      // Call the function to create a user with Firebase Authentication
+      var currentDate = new Date();
+      var dateString = currentDate.toISOString().substr(0, 10); // Format as YYYY-MM-DD
+      
+      console.log("Blog Date Append  == ",dateString)
+      console.log("User Id ==== ", userId);
+          writeUserData(userId, blogtitle,blogcontent, dateString)
+            .then(() => {
+                alert("Blog is Successfully Published");
+                window.location.href = "../dashboard/dashboard.html";
+              //
+            })
+            .catch((error) => {
+              console.error("Error Publishing Blog:", error);
+            });
+        
+      }
+    }
+  }
+
+  // Attach form validation function to the form's submit event
+  signupForm.addEventListener("submit", validateForm);
+
+  let writeUserData = (userId, blogtitle,blogcontent, date) => {
+    return new Promise((resolve, reject) => {
+        const userRef = ref(database, 'blogs/' + userId + `/${blogtitle}`);
+
+        set(userRef, {
+            userId: userId,
+            blogtitle: blogtitle,
+            blogcontent: blogcontent,
+            blogdate: date
+        })
+        .then(() => {
+            console.log("Blog saved to Firebase Database.");
+            resolve(); // Resolve the promise to indicate success
+        })
+        .catch((error) => {
+            console.error("Errori in Blog saving :", error);
+            reject(error); // Reject the promise with the error
+        });
+    });
+}
+
 } else if (userAcc !== null) {
   console.log("User is already logged In, did not required Login again");
   if (userAcc.acc_type === "blogger") {
