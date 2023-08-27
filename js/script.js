f1efe12bd33cb8de0b2871d9e93bd54d2ed7a7ba
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-27 10:20:59 +0500

    main p[age done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("JS script integrated");
+
+function openpage() {
+    let current_url = window.location;
+    console.log("current_url main === ", current_url);
+    setTimeout(() => {
+        current_url = current_url + 'pages/auth/signup.html';
+        console.log("current_url === ", current_url);
+        window.location.replace(current_url);
+
+    }, 2000);
+}
