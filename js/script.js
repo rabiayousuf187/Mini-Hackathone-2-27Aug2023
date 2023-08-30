4bc939b000b2b3a058cd4453c460969c50286658
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-30 15:17:20 +0500

    successfully added animation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ console.log("JS script integrated");
 
 function openpage() {
     let current_url = window.location;
+    open.play();
     console.log("current_url main === ", current_url);
     setTimeout(() => {
         current_url = current_url + 'pages/blogview.html';
@@ -11,45 +12,37 @@ function openpage() {
     }, 2000);
 }
 
-var changeEl = $(".el");
-var trimLine = anime.timeline({
-  loop: true
-});
-
-trimLine
-  .add({
-    targets: ".el",
-    width: [0, 160],
-    duration: 800,
-    easing: "easeOutQuart",
-    delay: 50
-  })
-  .add({
-    targets: ".el",
-    width: [160, 160],
-    easing: "linear",
-    duration: 800
-  })
-  .add({
-    targets: ".el",
-    width: [160, 0],
-    duration: 800,
-    easing: "easeOutQuart",
-    delay: 50
-  })
-  .add({
-    targets: ".el",
-    width: [0, 0],
-    easing: "linear",
-    duration: 800
+const burst = new mojs.Burst({
+    radius:   { 0: 300 },
+    count:    30,
+    children: {
+    //   shape:      'rect',
+      shape:      'polygon',
+      points:     30,
+      fill:       [ '#573e67', '#600f92' , '#cfcbd1' ],
+      rotate:      { 180: 0 },
+      radius:   8,
+      duration:   2000,
+      strokeWidth:  8,
+      delay:      'stagger(0, 100)'
+    }
   });
 
-trimLine.update = function(anim) {
-  if (anim.currentTime < 2500 && anim.currentTime > 850) {
-    changeEl.css({ float: "right", left: "auto", right: "20px" });
-  }
+  const open = new mojs.Burst({
+    radius:   { 0: 100 },
+    count:    10,
+    children: {
+      shape:      'polygon',
+      points:     5,
+      fill:       { 'purple' : 'white' },
+      rotate:      { 360: 0 },
+      duration:   2000,
+      delay:      'stagger(0, 100)'
+    }
+  });
 
-  if (anim.currentTime > 2500) {
-    changeEl.css({ float: "left", left: "20px", right: "auto" });
-  }
-};
+//   setInterval(  ()=>{
+//     // burst.generate();
+//     burst.play();
+//   }, 100);
+  burst.play();
