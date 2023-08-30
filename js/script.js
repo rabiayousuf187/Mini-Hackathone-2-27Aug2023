e1dac3e500ce14f2ec2861d1a16c4eec89362f19
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-08-30 12:44:20 +0500

    working on animation

--- js/script.js
+++ js/script.js
@@ -10,3 +10,46 @@ function openpage() {
 
     }, 2000);
 }
+
+var changeEl = $(".el");
+var trimLine = anime.timeline({
+  loop: true
+});
+
+trimLine
+  .add({
+    targets: ".el",
+    width: [0, 160],
+    duration: 800,
+    easing: "easeOutQuart",
+    delay: 50
+  })
+  .add({
+    targets: ".el",
+    width: [160, 160],
+    easing: "linear",
+    duration: 800
+  })
+  .add({
+    targets: ".el",
+    width: [160, 0],
+    duration: 800,
+    easing: "easeOutQuart",
+    delay: 50
+  })
+  .add({
+    targets: ".el",
+    width: [0, 0],
+    easing: "linear",
+    duration: 800
+  });
+
+trimLine.update = function(anim) {
+  if (anim.currentTime < 2500 && anim.currentTime > 850) {
+    changeEl.css({ float: "right", left: "auto", right: "20px" });
+  }
+
+  if (anim.currentTime > 2500) {
+    changeEl.css({ float: "left", left: "20px", right: "auto" });
+  }
+};
